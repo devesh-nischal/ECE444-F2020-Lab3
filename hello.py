e2cebdfa930fffc3aeb367f596d88b142df7ac8c
Devesh Nischal <devesh.nischal@gmail.com>
2020-09-29 21:41:27 -0400

    Commit for activity 1. Web app now displays time in LLLL format. Followed chapter 3 in textbook and added bootstrap into application.

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+from datetime import datetime
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+if __name__ == '__main__':
+	app.run(debug=True)
